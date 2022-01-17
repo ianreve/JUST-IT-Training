3fd10cb5d1aca3ff18ee26fd124630336284a670
ianreve <ianrevecacao@gmail.com>
2022-01-17 17:38:59 +0000

    format assignment fix

--- Python/5. Lists/listAssignmet17012022.py
+++ Python/5. Lists/listAssignmet17012022.py
@@ -1,27 +1,29 @@
 # ~list exercise
+# create a list of 6 items
 list1 = [1,2,3,4,5,6]
 print(list1)
 
-# create a list of 6 items
-list1.insert(2, "Third Position");
-print(list1)
-
 # insert a new item in postion 3
-list1.append(7)
+
+list1.insert(2, "Third Position")
 print(list1)
 
 # add another item to the list
+
+list1.append(7)
+print(list1)
+# remove an item by value
+
 list1.remove(5)
 print(list1)
 
-# remove an item by value
-del(list1[2])
-
 # remove the item at index position 3
+
+
+del(list1[2])
 print(list1)
 
 # for every list manipulation print the list
-
 
 
 
