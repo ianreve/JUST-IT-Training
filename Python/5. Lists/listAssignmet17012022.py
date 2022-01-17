f22fde88017ab4012d668498c97391eaa84453f1
ianreve <ianrevecacao@gmail.com>
2022-01-17 17:24:39 +0000

    17/01/2022 -  list assignment = done

---
+++ Python/5. Lists/listAssignmet17012022.py
@@ -0,0 +1,31 @@
+# ~list exercise
+list1 = [1,2,3,4,5,6]
+print(list1)
+
+# create a list of 6 items
+list1.insert(2, "Third Position");
+print(list1)
+
+# insert a new item in postion 3
+list1.append(7)
+print(list1)
+
+# add another item to the list
+list1.remove(5)
+print(list1)
+
+# remove an item by value
+del(list1[2])
+
+# remove the item at index position 3
+print(list1)
+
+# for every list manipulation print the list
+
+
+
+
+
+
+
+
